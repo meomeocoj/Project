31b3c3b9a3dceee6b9255826526d6fff1575d3d2
miasnguyenx <miasnguyenx@gmail.com>
2021-05-31 18:35:40 +0700

    add sign up controller

---
+++ controller/signupPageController.php
@@ -0,0 +1,26 @@
+<?php
+class homePageController extends Controller
+{
+    public function index()
+    {
+        require 'model/user.php';
+        $user = new User;
+        $signupPage = $this->render("signupPage");
+        echo $signupPage;
+        
+        if (isset($_POST['signup']) == true) {
+            $data = array();
+            $data['fullname'] = $_POST['fullname'];
+            $data['email'] = $_POST['email'];
+            $data['username'] = $_POST['username'];
+            $data['password'] = $_POST['password'];
+
+            if ($user->insert($data)) {
+                redirect('index.php', 'Your account has been created', 'success');
+            } else {
+                redirect('index.php', 'Something went wrong', 'error');
+            }
+        }
+        
+    }
+}
