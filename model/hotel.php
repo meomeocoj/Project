1882cc0654e7a2f7c5aa44a4e84454aa9de406dc
qqhuyorai <78142250+qqhuyorai@users.noreply.github.com>
2021-06-07 21:25:01 +0700

    add detailPage

--- model/hotel.php
+++ model/hotel.php
@@ -28,15 +28,28 @@ class Hotel
     }
 
     public function getHotelById($id, $page) {
-        $this->db->query("SELECT DISTINCT url.hotel_id, hotel.name, MAX(url.min_price) AS 'price' FROM `hotel`, `url` WHERE hotel.province_id = :id AND url.hotel_id = hotel.id GROUP BY url.hotel_id LIMIT :offset, 20");
+        $this->db->query("SELECT DISTINCT url.hotel_id, hotel.name, MAX(url.min_price) AS 'price' FROM `hotel`, `url` WHERE hotel.province_id = :id AND url.hotel_id = hotel.id GROUP BY url.hotel_id LIMIT :offset, 11");
         $this->db->bind(':id', $id);
-        $this->db->bind(':offset', ($page-1)*20);
+        $this->db->bind(':offset', ($page-1)*11);
         $results = $this->db->resultSet();
         return $results;
     }
 
     public function getProvinceById($id) {
         $this->db->query("SELECT name FROM `province` WHERE id = :id");
+        $this->db->bind(':id', $id);
+        $result = $this->db->result();
+        return $result;
+    }
+
+    public function getNumberOfHotelById($id) {
+        $this->db->query("SELECT COUNT(*) as 'noOfHotel' FROM `hotel` WHERE province_id = :id");
+        $this->db->bind(':id', $id);
+        $result = $this->db->result();
+        return $result;
+    }
+    public function getHotelInfoById($id) {
+        $this->db->query("SELECT hotel.*, MAX(url.min_price) AS 'price', province.name AS 'province' FROM `hotel`, `url`, `province` WHERE hotel.id = :id AND url.hotel_id = hotel.id AND province.id = hotel.province_id");
         $this->db->bind(':id', $id);
         $result = $this->db->result();
         return $result;
--- view/pagination/pagination.php
+++ view/pagination/pagination.php
@@ -14,9 +14,9 @@
     <?php include '../common/header.php'; ?>
     <div class="container">
         <div class="filter">
-            <!--        <div id="total">-->
-            <!--            $total hotel available in $location-->
-            <!--        </div>-->
+                    <div id="total">
+                        <?php echo $number->noOfHotel.' hotels available in '.$location->name; ?>
+                    </div>
             <div id="filter">
                 <span>Price </span>
                 <select name="price-filter" onchange="filter(this.value)">
@@ -30,8 +30,9 @@
         <?php
             foreach($attribute as $att) {
                 if($att->price == -1) {
-                    $att->price = rand(500000,1500000).'.0';
+                    $att->price = rand(500000,1500000);
                 }
+                $att->price = ceil($att->price/1000)*1000;
                 echo '<div id="box"><a href="http://localhost/Project/detail/show/?id='.$att->hotel_id.'"><img class ="image" src="../../view/image/hotel.jpg" alt="house" style="width:300px; height:200px"></a><a href="http://localhost/Project/detail/show/?id='.$att->hotel_id.'">' . $att->name . '</a><p>'.$att->price.'</p></div>';
             }
             ?>
