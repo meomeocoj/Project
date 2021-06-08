b9c51f3c02897d1bfac0f8b5553310f09c4b173e
khoi.vt <khoivutrong1212@gmail.com>
2021-06-08 19:14:00 +0700

    search complete

---
+++ view/common/searchresult.php
@@ -0,0 +1,23 @@
+<?php
+$connection = mysqli_connect('localhost', 'root', '');
+if(!$connection){
+    die("Database Connection Failed" . mysqli_error($connection));
+}
+$selectdb = mysqli_select_db($connection, 'hotel');
+if(!$selectdb){
+    die("Database Selection Failed" . mysqli_error($connection));
+}
+
+$search = $_GET['search'];
+$sql = "SELECT DISTINCT hotel.name, hotel.address, hotel.province_id FROM hotel, province WHERE province.id = hotel.province_id AND province.name LIKE '%$search%' ORDER BY name DESC LIMIT 0,6";
+$result = mysqli_query($connection, $sql);
+$count = 0;
+while($row = mysqli_fetch_assoc($result)){
+    if ($count != 5) {
+        echo "<li><a href='#'>".$row['name']."</a>";
+    } else {
+        echo "<li hidden id='province_id'>pagination/show/?id=".$row['province_id']."&page=1</a>";
+    }
+    $count++;
+}
+?>
